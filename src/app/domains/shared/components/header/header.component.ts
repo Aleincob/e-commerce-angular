bfb368203b3d3f0e4ccf508e20d747ba12126f41
Alejandro Jacobo <alecaperez13@gmail.com>
2024-03-15 13:53:52 -0500

    Carrito de compras (sideMenu) agregado

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,4 +8,10 @@ import { CommonModule } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  hideSideMenu = signal(true);
+
+  toogleSideMenu() {
+    this.hideSideMenu.update((prevState) => !prevState);
+  }
+}
